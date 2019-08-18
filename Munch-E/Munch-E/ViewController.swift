8de42ce9ec392efc12da922cd7fbe7e7406d3f45
gilr00y <jas.gilbertson@gmail.com>
2019-08-18 14:48:03 -0500

    Add test ONNX model. Add pre-trained StarryNight.

--- Munch-E/Munch-E/ViewController.swift
+++ Munch-E/Munch-E/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var selectedImg: UIImageView!
+    
     @IBAction func btnClick(_ sender: UIButton) {
-        print("Button clicked")
         print("Opening Image Picker")
         let picker = UIImagePickerController()
         picker.allowsEditing = true
@@ -25,11 +26,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imagePicker.delegate = self
             imagePicker.sourceType = .camera
             // Prevent user from editing/cropping the photo
-            imagePicker.allowsEditing = false
+            imagePicker.allowsEditing = true
             self.present(imagePicker, animated: true, completion: nil)
         } else {
             print("Camera not available.")
         }
+    }
+    @IBAction func munchify(_ sender: Any) {
+        //
+        print("MUNCHIFYING IMAGE: \(selectedImg.image!)")
+//        Save the input image size
+//        Convert the input UIImage to a 720 x 720 CVPixelBuffer, as specified in the model view.
+//        Feed it to our Model
+//        Convert the CVPixelBuffer output to an UIImage
+//        Resize it to the original size saved at the first step
+
     }
     
     override func viewDidLoad() {
@@ -48,6 +59,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             print("Failed to import UIImage for processing...")
             return
         }
+        picker.delegate = self
+        selectedImg.contentMode = UIView.ContentMode.scaleAspectFill
+        selectedImg.image = image
         let imageName = UUID().uuidString
         print("IMAGE NAME: \(imageName)")
         let imagePath =
