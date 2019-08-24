9884d1936cda4ca5becf31859fe703a1dddcb7fb
gilr00y <jas.gilbertson@gmail.com>
2019-08-24 12:33:54 -0500

    Add save-ability.

--- Munch-E/Munch-E/ViewController.swift
+++ Munch-E/Munch-E/ViewController.swift
@@ -21,6 +21,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(picker, animated: true)
     }
     
+    @IBAction func saveImage(_ sender: Any) {
+        guard let imgToSave = selectedImg.image else {
+            print("Must select an image to Munch-ify!")
+            return
+        }
+        
+        UIImageWriteToSavedPhotosAlbum(imgToSave, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            print("IMG SAVED... Maybe")
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
     @IBAction func openCameraButton(_ sender: UIButton) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let imagePicker = UIImagePickerController()
